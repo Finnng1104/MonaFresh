3f9925fe1af8ca8df34c743f8ca42f9fc32806df
Finnng1104 <tiennt1104@gmail.com>
2024-07-11 17:40:15 +0700

    add contact page

--- view/php/contact.php
+++ view/php/contact.php
@@ -4,37 +4,53 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="upload/css/contact.css">
 </head>
 <body>
     <div class="banner-item">
-        <h2><b>LIÊN HỆ</b></h2>
+        <h2>LIÊN HỆ</h2>
         TRANG CHỦ / <b>LIÊN HỆ</b>
     </div>
     <div class="container">
-        <div class="float-left">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1959.222066284073!2d106.62368036806588!3d10.853785163716562!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752b6c59ba4c97%3A0x535e784068f1558b!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYw!5e0!3m2!1svi!2s!4v1706496866340!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
-        <div class="float-right">
-            <div class="form-group-title">
-                <h3><b>LIÊN HỆ VỚI CHÚNG TÔI</b></h3>
-              </div>
-            <form action="" method="">
-                <div class="form-group">
-                    <div class="form-group-item">
-                        <input type="text" placeholder="Họ Và Tên" name="name" required>  
-                        <input type="text" placeholder="Email" name="Email" required>  
-                        <input type="text" placeholder="Số điện thoại" name="phonenumber" required>  
-                        <input type="text" placeholder="Địa chỉ" name="address" required>  
-                        
-                    </div>
-                    <div class="form-group-item">
-                        <textarea id="textarea1" placeholder="Những điều mà bạn muốn chia sẻ với chúng tôi..." name="textarea1"></textarea>
-                    </div>
-        
-              
-                  <button id="submit-button" type="submit">GỬI</button>
+        <div class="contact-content">
+            <div class="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1959.222066284073!2d106.62368036806588!3d10.853785163716562!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752b6c59ba4c97%3A0x535e784068f1558b!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYw!5e0!3m2!1svi!2s!4v1706496866340!5m2!1svi!2s" width="600" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="contact-from">
+                <div class="d-flex contact-infor">
+                    <img src="upload/img/blogs/logo-mona.png" alt="">
+                    <address>
+                        <nav>
+                            <ul>
+                                <li><i class="fa-solid fa-location-dot"></i> 319 C16 Lý Thường Kiệt, P15, Q11, Tp.HCM</li>
+                                <li><i class="fa-solid fa-phone"></i><a href="#"> Điện thoại: 076 922 0162</a></li>
+                                <li><i class="fa-solid fa-envelope"></i><a href="#"> demonhunterg@gmail.com</a></li>
+                                <li><i class="fa-brands fa-skype"></i><a href="#">demonhunterp</a></li>
+                            </ul>
+                        </nav>
+                    </address>
                 </div>
-              </form>
+                <div class="form-group-title">
+                    <h3><b>LIÊN HỆ VỚI CHÚNG TÔI</b></h3>
+                </div>
+                <form action="" method="">
+                    <div class="form-group">
+                        <div class="form-group-item">
+                            <input type="text" placeholder="Họ Và Tên" name="name" required>  
+                            <input type="text" placeholder="Email" name="Email" required>  
+                            <input type="text" placeholder="Số điện thoại" name="phonenumber" required>  
+                            <input type="text" placeholder="Địa chỉ" name="address" required>  
+                        </div>
+                        <div class="form-group-item">
+                            <textarea id="textarea1" placeholder="Những điều mà bạn muốn chia sẻ với chúng tôi..." name="textarea1"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group-title">
+                        <button id="submit-button" type="submit">GỬI</button>
+                    </div>
+                </form>
+               
+            </div>
         </div>
     </div>
 </body>
