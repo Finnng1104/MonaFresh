ec97edaaf6ac6c2ca0054a827750af62580d1d92
Finnng1104 <tiennt1104@gmail.com>
2024-09-15 16:27:43 +0700

    Reapply "add database"
    This reverts commit 6fa20d070c08eb640e2416d15f6d730e6c7b3a76.

---
+++ database/migrations/2024_09_04_141628_create_orders_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('phone_number', 20);
+            $table->string('address');
+            $table->timestamp('order_date')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
